5ba7d8a4031d7a1bf2032cbd118dd9a76bc697ed
thomas <thomas@Gimpel.local>
2017-05-05 20:37:54 +0200

    Fixed the build batch files to work with the new directory structure

--- examples/gbr.bat
+++ examples/gbr.bat
@@ -3,8 +3,10 @@
 rem Batch file for building and executing Gnuplot/C example programs with GCC
 rem This also rebuilds the library
 
+LIBDIR=../src
+
 del %1.exe
-gcc %1.c -Wall -std=c99 -I .. -I .. -l gnuplot_c -L .. -o %1.exe
+gcc %1.c -Wall -std=c99 -I $(LIBDIR) -l $(LIBDIR)/gnuplot_c -L $(LIBDIR)/src -o %1.exe
 
 if exist %1.exe %1
 
--- examples/mbr.bat
+++ examples/mbr.bat
@@ -7,14 +7,19 @@ del %1.exe
 del %1.obj
 
 REM set SRCDIR=%CD%
-REM cd ..
+REM set LIBDIR=../src
+
+:: Rebuild the library
+REM cd %LIBDIR%
 REM del gnuplot_c.obj
 REM del gnuplot_c.lib
 REM cl -c -W4 -D "_CRT_SECURE_NO_WARNINGS=1" gnuplot_c.c
 REM lib /NOLOGO /OUT:gnuplot_c.lib gnuplot_c.obj
+
+:: Build the application
 REM chdir /d %SRCDIR%
 
-cl %1.c -W4 -D "SIGLIB_STATIC_LIB=1" -D "_CRT_SECURE_NO_WARNINGS=1" gnuplot_c.lib
+cl %1.c -W4 -D "SIGLIB_STATIC_LIB=1" -D "_CRT_SECURE_NO_WARNINGS=1" /I %LIBDIR% "%LIBDIR%/gnuplot_c.lib"
 
 del *.obj
 if exist %1.exe %1
